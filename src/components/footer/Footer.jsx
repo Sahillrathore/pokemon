94d47532d12291a3f683edfc07374b65be355cc1
Sahillrathore <sahil921031@gmail.com>
2024-07-26 21:23:03 +0530

    feat: git-link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='px-20 py-4 border-t-2'>
-        <p className='capitalize text-lg font-medium text-center w-full'>made by <span className='uppercase'>sahil</span> with &#10084;</p>
+    <footer className='px-20 py-4 border-t-2 relative'>
+      <p className='capitalize text-lg font-medium text-center w-full'>made by <span className='uppercase'>sahil</span> with &#10084;</p>
+
+      <div className="git-logo absolute bottom-2 right-10">
+
+        <a href="https://github.com/Sahillrathore/pokemon" target='_blank'>
+          <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+            className='h-10'
+            alt="" />
+        </a>
+      </div>
     </footer>
   )
 }
